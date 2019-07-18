c387090e0162badb87d35c62fef76c9925a356f7
cabanga <manucabanga@gmail.com>
2019-07-18 16:58:00 +0100

    update

--- src/views/Jobs/New-Job.vue
+++ src/views/Jobs/New-Job.vue
@@ -45,7 +45,7 @@
               <div class="row form-group mb-5">
                 <div class="col-md-12 mb-3 mb-md-0">
                   <label class="font-weight-bold" for="company">Instituição Empresárial</label>
-                  <input v-model="vacancy.company" type="text" id="company" class="form-control" placeholder="eg. Facebook, Inc.">
+                  <input v-model="vacancy.company" type="text" id="company" class="form-control" placeholder="ex. Moon LDA, Inc.">
                 </div>
               </div>
 
@@ -83,14 +83,14 @@
               </div>
 
               <div class="row form-group mb-4">
-                <div class="col-md-12"><h3>Location</h3></div>
+                <div class="col-md-12"><h3>Localização</h3></div>
                 <div class="col-md-12 mb-3 mb-md-0">
-                  <input v-model="vacancy.location" type="text" class="form-control" placeholder="Western City, UK">
+                  <input v-model="vacancy.location" type="text" class="form-control" placeholder="Samba, rua 2">
                 </div>
               </div>
 
               <div class="row form-group">
-                <div class="col-md-12"><h3>Job Description</h3></div>
+                <div class="col-md-12"><h3>Descrição da Vaga</h3></div>
                 <div class="col-md-12 mb-3 mb-md-0">
                   <textarea v-model="vacancy.description" name="" class="form-control" id="" cols="30" rows="5"></textarea>
                 </div>
@@ -101,7 +101,6 @@
                 <div class="col-md-12">
                   <input type="submit" value="Publicar Nova Vaga" class="btn btn-primary  py-2 px-5">
                   <router-link class="btn btn-secondary  py-2 px-5" :to="{name: 'dashboard'}">Cancelar</router-link>
-
                 </div>
               </div>
 
@@ -110,22 +109,13 @@
 
           <div class="col-lg-4">
             <div class="p-4 mb-3 bg-white">
-              <h3 class="h5 text-black mb-3">Contact Info</h3>
-              <p class="mb-0 font-weight-bold">Address</p>
-              <p class="mb-4">203 Fake St. Mountain View, San Francisco, California, USA</p>
-
-              <p class="mb-0 font-weight-bold">Phone</p>
-              <p class="mb-4"><a href="#">+1 232 3235 324</a></p>
-
-              <p class="mb-0 font-weight-bold">Email Address</p>
-              <p class="mb-0"><a href="#"><span class="__cf_email__" data-cfemail="671e081215020a060e0b2703080a060e094904080a">[email&#160;protected]</span></a></p>
-
-            </div>
-            
-            <div class="p-4 mb-3 bg-white">
-              <h3 class="h5 text-black mb-3">More Info</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa ad iure porro mollitia architecto hic consequuntur. Distinctio nisi perferendis dolore, ipsa consectetur</p>
-              <p><a href="#" class="btn btn-primary  py-2 px-4">Learn More</a></p>
+              <h3 class="h5 text-black mb-3">Contacto Info</h3>
+              <p class="mb-0 font-weight-bold">Endereço</p>
+              <p class="mb-4">Pedro de Castro Vandune-Loy, Belas, Luanda, Angola</p>
+              <p class="mb-0 font-weight-bold">Telemóvel</p>
+              <p class="mb-4"><a href="#">+244 913 747 220</a></p>
+              <p class="mb-0 font-weight-bold">Endereço Email</p>
+              <p class="mb-0"><a href="#"><span class="__cf_email__">suport@moon.ao</span></a></p>
             </div>
           </div>
 
